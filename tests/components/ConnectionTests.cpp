7d14edb692fa32f97a6b9bf2899c63bd67909e32
theo.baudoin30@gmail.com <theo.baudoin30@gmail.com>
2025-05-16 15:16:24 +0200

    -> Advanced Node Editor :
    Fix connections issues

--- tests/components/ConnectionTests.cpp
+++ tests/components/ConnectionTests.cpp
@@ -6,101 +6,106 @@ using namespace NodeEditorCore;
 class ConnectionTests : public ::testing::Test {
 protected:
     NodeEditor editor;
-    
+    int outputPinId;
+    int inputPinId;
+
     void SetUp() override {
         editor.addNode("Node1", "Default", Vec2(100, 100));
         editor.addNode("Node2", "Default", Vec2(300, 100));
-        
-        editor.addPin(1, "Output", false, PinType::Blue);
-        editor.addPin(2, "Input", true, PinType::Blue);
+
+        outputPinId = editor.addPin(1, "Output", false, PinType::Blue);
+        inputPinId = editor.addPin(2, "Input", true, PinType::Blue);
+
+        ASSERT_NE(outputPinId, -1);
+        ASSERT_NE(inputPinId, -1);
     }
 };
 
 TEST_F(ConnectionTests, AddConnection) {
-    int connectionId = editor.addConnection(1, 1, 2, 1);
+    int connectionId = editor.addConnection(1, outputPinId, 2, inputPinId);
     EXPECT_EQ(connectionId, 1);
-    
+
     const Connection* connection = editor.getConnection(connectionId);
     ASSERT_NE(connection, nullptr);
     EXPECT_EQ(connection->startNodeId, 1);
-    EXPECT_EQ(connection->startPinId, 1);
+    EXPECT_EQ(connection->startPinId, outputPinId);
     EXPECT_EQ(connection->endNodeId, 2);
-    EXPECT_EQ(connection->endPinId, 1);
+    EXPECT_EQ(connection->endPinId, inputPinId);
     EXPECT_FALSE(connection->selected);
 }
 
 TEST_F(ConnectionTests, GetConnection) {
-    editor.addConnection(1, 1, 2, 1);
-    
+    int connectionId = editor.addConnection(1, outputPinId, 2, inputPinId);
+
     const Connection* connection = editor.getConnection(1);
     ASSERT_NE(connection, nullptr);
     EXPECT_EQ(connection->id, 1);
-    
+
     const Connection* nonExistentConnection = editor.getConnection(999);
     EXPECT_EQ(nonExistentConnection, nullptr);
 }
 
 TEST_F(ConnectionTests, RemoveConnection) {
-    int connectionId = editor.addConnection(1, 1, 2, 1);
-    
+    int connectionId = editor.addConnection(1, outputPinId, 2, inputPinId);
+
     editor.removeConnection(connectionId);
-    
+
     const Connection* connection = editor.getConnection(connectionId);
     EXPECT_EQ(connection, nullptr);
 }
 
 TEST_F(ConnectionTests, IsConnected) {
-    editor.addConnection(1, 1, 2, 1);
-    
-    EXPECT_TRUE(editor.isConnected(1, 1));
-    EXPECT_TRUE(editor.isConnected(2, 1));
+    editor.addConnection(1, outputPinId, 2, inputPinId);
+
+    EXPECT_TRUE(editor.isConnected(1, outputPinId));
+    EXPECT_TRUE(editor.isConnected(2, inputPinId));
     EXPECT_FALSE(editor.isConnected(1, 999));
 }
 
 TEST_F(ConnectionTests, ConnectionExistenceTests) {
-    int connectionId = editor.addConnection(1, 1, 2, 1);
+    int connectionId = editor.addConnection(1, outputPinId, 2, inputPinId);
     EXPECT_NE(connectionId, -1);
 
-    int duplicateId = editor.addConnection(1, 1, 2, 1);
+    int duplicateId = editor.addConnection(1, outputPinId, 2, inputPinId);
     EXPECT_EQ(duplicateId, -1);
 
-    int nonExistentPinId = editor.addConnection(1, 999, 2, 1);
+    int nonExistentPinId = editor.addConnection(1, 999, 2, inputPinId);
     EXPECT_EQ(nonExistentPinId, -1);
 
-    int reverseId = editor.addConnection(2, 1, 1, 1);
+    int reverseId = editor.addConnection(2, inputPinId, 1, outputPinId);
     EXPECT_EQ(reverseId, -1);
 }
 
 TEST_F(ConnectionTests, ConnectionByUUID) {
     UUID node1Uuid = editor.getNodeUUID(1);
     UUID node2Uuid = editor.getNodeUUID(2);
-    UUID pin1Uuid = editor.getPinUUID(1, 1);
-    UUID pin2Uuid = editor.getPinUUID(2, 1);
-    
+    UUID pin1Uuid = editor.getPinUUID(1, outputPinId);
+    UUID pin2Uuid = editor.getPinUUID(2, inputPinId);
+
     UUID connectionUuid = editor.addConnectionWithUUIDByUUID(node1Uuid, pin1Uuid, node2Uuid, pin2Uuid);
     EXPECT_FALSE(connectionUuid.empty());
-    
+
     const Connection* connection = editor.getConnectionByUUID(connectionUuid);
     ASSERT_NE(connection, nullptr);
     EXPECT_EQ(connection->startNodeId, 1);
-    EXPECT_EQ(connection->startPinId, 1);
+    EXPECT_EQ(connection->startPinId, outputPinId);
     EXPECT_EQ(connection->endNodeId, 2);
-    EXPECT_EQ(connection->endPinId, 1);
-    
+    EXPECT_EQ(connection->endPinId, inputPinId);
+
     editor.removeConnectionByUUID(connectionUuid);
     connection = editor.getConnectionByUUID(connectionUuid);
     EXPECT_EQ(connection, nullptr);
 }
 
 TEST_F(ConnectionTests, SelectConnection) {
-    int connectionId = editor.addConnection(1, 1, 2, 1);
-    
+    int connectionId = editor.addConnection(1, outputPinId, 2, inputPinId);
+
     editor.selectConnection(connectionId, false);
-    
+
     const Connection* connection = editor.getConnection(connectionId);
     ASSERT_NE(connection, nullptr);
     EXPECT_TRUE(connection->selected);
-    
+
     editor.deselectConnection(connectionId);
     connection = editor.getConnection(connectionId);
     ASSERT_NE(connection, nullptr);
@@ -108,11 +113,11 @@ TEST_F(ConnectionTests, SelectConnection) {
 }
 
 TEST_F(ConnectionTests, DeselectAllConnections) {
-    int conn1 = editor.addConnection(1, 1, 2, 1);
+    int conn1 = editor.addConnection(1, outputPinId, 2, inputPinId);
 
-    editor.addPin(1, "Output2", false, PinType::Red);
-    editor.addPin(2, "Input2", true, PinType::Red);
-    int conn2 = editor.addConnection(1, 2, 2, 2);
+    int outputPinId2 = editor.addPin(1, "Output2", false, PinType::Red);
+    int inputPinId2 = editor.addPin(2, "Input2", true, PinType::Red);
+    int conn2 = editor.addConnection(1, outputPinId2, 2, inputPinId2);
 
     editor.selectConnection(conn1, false);
     editor.selectConnection(conn2, true);
